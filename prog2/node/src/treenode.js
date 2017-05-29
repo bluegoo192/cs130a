a935247c3031ee90470f46c0e4f89c46cce42423
Arthur Silverstein(bluegoo192) <silversteinarthur@gmail.com>
2017-05-29 12:29:16 -0700

    moved verification code to verify.js

--- prog2/node/src/treenode.js
+++ prog2/node/src/treenode.js
@@ -21,24 +21,6 @@ function Node(data, parent = null) {
     }
 }
 
-Node.prototype.verify = function() {
-    //ONLY VERIFIES THE CURRENT NODE.  DOES NOT CHECK CHILDREN
-    var correct = true
-    if (!(typeof this.left === 'undefined' || this.left === null)) {
-        if (this.left.data >= this.data) {
-            correct = false;
-            console.log("Something's wrong. left("+this.left.data+") >= data("+this.data+")");
-        }
-    }
-    if (!(typeof this.right === 'undefined' || this.right === null)) {
-        if (this.right.data <= this.data) {
-            correct = false;
-            console.log("Something's wrong. right("+this.right.data+") <= data("+this.data+")");
-        }
-    }
-    return correct;
-}
-
 Node.prototype.access = function(target) {
     if (this.data === target) { //if we reached our target
         this.splay();           //splay
--- prog2/node/tests/verify.js
+++ prog2/node/tests/verify.js
@@ -1,3 +1,23 @@
+var Node = require('../src/treenode.js');
+
+Node.prototype.verify = function() {
+    //ONLY VERIFIES THE CURRENT NODE.  DOES NOT CHECK CHILDREN
+    var correct = true
+    if (!(typeof this.left === 'undefined' || this.left === null)) {
+        if (this.left.data >= this.data) {
+            correct = false;
+            console.log("Something's wrong. left("+this.left.data+") >= data("+this.data+")");
+        }
+    }
+    if (!(typeof this.right === 'undefined' || this.right === null)) {
+        if (this.right.data <= this.data) {
+            correct = false;
+            console.log("Something's wrong. right("+this.right.data+") <= data("+this.data+")");
+        }
+    }
+    return correct;
+}
+
 function verify(node) {
     if (typeof node === 'undefined' || node === null) return true;
     if (!node.verify()) return false;
